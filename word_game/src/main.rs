0f6d12c0e930c3e24de7fd4fef085baf2db5dfb0
RedIsGaming <jaredhofman@protonmail.com>
2024-05-22 02:45:47 +0200

    feat: Splitted code and menu improved.

--- word_game/src/main.rs
+++ word_game/src/main.rs
@@ -1,40 +1,7 @@
-use std::{fmt, process};
 use std::process::{Command, ExitStatus};
-use std::str::FromStr;
 use std::error;
 use std::io::{stdout, stdin, Write};
-
-#[derive(Debug)]
-enum Menu {
-    Wordgame,
-    Prank,
-    Quit
-}
-
-impl fmt::Display for Menu {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {        
-        match self {
-            Menu::Wordgame => write!(f, "1. Wordgame"),
-            Menu::Prank => write!(f, "2. Prank"),
-            Menu::Quit => write!(f, "3. Quit"),
-        }
-    }
-}
-
-impl Menu {
-    fn play() -> () {
-        println!("Play!")
-    }
-
-    fn prank() -> () {
-        println!("Prank")
-    }
-
-    fn quit() -> () {
-        println!("Goodbye!!!");
-        process::exit(0)
-    }
-}
+use word_game::*;
 
 #[derive(Debug)]
 struct Wordgame;
@@ -53,44 +20,33 @@ impl Wordgame {
         let mut input = String::new();
         
         stdout().lock().flush()?;
-        Wordgame::print_menu();
+        Menu::print_menu();
         stdin().read_line(&mut input)?;
 
         let option = Field::parse(&input);
-        Wordgame::menu(option);
+        Wordgame::menu(option, input);
         Ok(())
     }
 
-    fn print_menu() -> () {
-        println!("{}\n{}\n{}", Menu::Wordgame, Menu::Prank, Menu::Quit)
-    }
-
-    fn menu(option: u8) -> () {
+    fn menu(option: u8, input: String) -> () {
         match option {
             1 => Menu::play(),
             2 => Menu::prank(),
             3 => Menu::quit(),
-            _ => panic!("Invalid option chosen..."),
+            _ => Wordgame::default(input),
         }
     }
-}
 
-#[derive(Debug)]
-struct Field;
+    fn default(mut input: String) -> () {
+        input.clear();
 
-impl Field {
-    fn parse(s: &str) -> u8 {
-        let parse = s.trim()
-            .parse::<u8>()
-            .map_err(|_| format!("Error while trying to parse to u8."))
-            .expect("Expected a valid whole number between 0-255");
+        Wordgame::clear();
+        let _ = stdout().write_all(b"Determine an option for Reddy Wordgame:\n");
+        Menu::print_menu();
 
-        let input = parse.to_string();
-
-        match u8::from_str(&input) {
-            Ok(num) => num,
-            Err(_) => 0,
-        }
+        stdin().read_line(&mut input).unwrap();
+        let option = Field::parse(&input);
+        Wordgame::menu(option, input);
     }
 }
 
